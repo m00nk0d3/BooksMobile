b10f2bb2be3310c774d59a936ab83e84b06f5a48
Joao Martins <joaopaulomartinspessoal@gmail.com>
2023-02-04 03:01:25 -0100

    added tabs

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,4 +1,4 @@
-import { Button, StyleSheet } from 'react-native';
+import { Button, Pressable, StyleSheet } from 'react-native';
 import {useLazyQuery, useQuery} from '@apollo/client'
 import EditScreenInfo from '../components/EditScreenInfo';
 import {ActivityIndicator, FlatList, TextInput} from 'react-native' 
@@ -8,24 +8,43 @@ import {SearchBooksQuery} from '../apollo/queries/SearchBooks'
 import BookItem from '../components/BookItem';
 import { useState } from 'react';
 
+type Book = {
+  title:string,
+  image: string,
+  authors: string[],
+  isbn:string
+}
+
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
 
   const [search, setSearch] = useState<string>('')
+  const [provider, setProvider] = useState<"googleBooksSearch" | "openLibrarySearch">("googleBooksSearch")
 	
   const [runQuery,{data, loading, error}] = useLazyQuery(SearchBooksQuery,{
 			variables: {q: search},
 		})
 
+  const parseBook = (item):Book => {
+    if(provider === "googleBooksSearch"){
+      return {
+        title: item.volumeInfo.title,
+        image: item.volumeInfo.imageLinks?.thumbnail,
+        authors: item.volumeInfo.authors,
+        isbn: item.volumeInfo.industryIdentifiers?.[0]?.identifier,
+      }
+    }
+    return {
+      image: `https://covers.openlibrary.org/b/olid/${item.cover_edition_key}-M.jpg`,
+      title: item.title,
+      authors: item.author_name,
+      isbn: item.isbn?.[0],
+    }
+  }
   
   const renderItem = ({item})=>{
     return (
       <BookItem
-        book={{
-          title: item.volumeInfo.title,
-          image: item.volumeInfo.imageLinks?.thumbnail,
-          authors: item.volumeInfo.authors,
-          isbn: item.volumeInfo.industryIdentifiers?.[0]?.identifier,
-        }}
+        book={parseBook(item)}
       />
     )
   }
@@ -44,6 +63,16 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
           onPress={()=> runQuery({variables: {q:search}})}
           />
       </View>
+      <View style={styles.tabs}>
+        <Pressable onPress={()=>setProvider("googleBooksSearch")}>
+
+        <Text style={provider==="googleBooksSearch"?{fontWeight: "bold", color:"royalblue"}:{}}>Google Books</Text>
+        </Pressable>
+        <Pressable onPress={()=>setProvider("openLibrarySearch")}>
+
+        <Text style={provider==="openLibrarySearch"?{fontWeight: "bold", color:"royalblue"}:{}} >Open Library</Text>
+        </Pressable>
+      </View>
 		  {loading && <ActivityIndicator />}
 		  {error &&(
 			  <View style={styles.container}>
@@ -52,7 +81,7 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
 			  </View>
 	  	)}
     <FlatList 
-      data={data?.googleBooksSearch?.items || []}
+      data={(provider === "googleBooksSearch"? data?.googleBooksSearch?.items : data?.openLibrarySearch?.docs) || []}
       renderItem={renderItem as any}
       showsVerticalScrollIndicator={false}
     />
@@ -64,6 +93,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 10
+  },
+  tabs: {
+    flexDirection: 'row',
+    justifyContent: "space-around",
+    height: 50,
+    alignItems:"center"
   },
   header:{
     flexDirection:"row",  
