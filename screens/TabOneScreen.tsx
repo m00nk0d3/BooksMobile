0ab9efcaf43bae66d17eb53d26b40942211ff61a
Joao Martins <joaopaulomartinspessoal@gmail.com>
2023-02-04 01:46:02 -0100

    Added search

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,16 +1,19 @@
-import { StyleSheet } from 'react-native';
-import {useQuery} from '@apollo/client'
+import { Button, StyleSheet } from 'react-native';
+import {useLazyQuery, useQuery} from '@apollo/client'
 import EditScreenInfo from '../components/EditScreenInfo';
-import {ActivityIndicator, FlatList} from 'react-native' 
+import {ActivityIndicator, FlatList, TextInput} from 'react-native' 
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 import {SearchBooksQuery} from '../apollo/queries/SearchBooks'
 import BookItem from '../components/BookItem';
+import { useState } from 'react';
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+
+  const [search, setSearch] = useState<string>('')
 	
-  const {data, loading, error} = useQuery(SearchBooksQuery,{
-			variables: {q: "React Native"},
+  const [runQuery,{data, loading, error}] = useLazyQuery(SearchBooksQuery,{
+			variables: {q: search},
 		})
 
   
@@ -21,7 +24,7 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
           title: item.volumeInfo.title,
           image: item.volumeInfo.imageLinks?.thumbnail,
           authors: item.volumeInfo.authors,
-          isbn: item.volumeInfo.industryIdentifiers[0].identifier,
+          isbn: item.volumeInfo.industryIdentifiers?.[0]?.identifier,
         }}
       />
     )
@@ -29,6 +32,18 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
 	console.log(JSON.stringify(data,null, 2))
   return (
     <View style={styles.container}>
+      <View style={styles.header}>
+        <TextInput 
+        style={styles.input} 
+        placeholder='Search'
+        onChangeText={setSearch}
+        value={search}
+        />
+        <Button 
+          title='Search' 
+          onPress={()=> runQuery({variables: {q:search}})}
+          />
+      </View>
 		  {loading && <ActivityIndicator />}
 		  {error &&(
 			  <View style={styles.container}>
@@ -49,6 +64,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 10
+  },
+  header:{
+    flexDirection:"row",  
+    alignItems:'center'
+  },
+  input:{
+    flex: 1,
+    borderWidth:1,
+    borderRadius: 40,
+    borderColor: "gainsboro",
+    paddingHorizontal:20,
+    marginRight: 10,
+    marginVertical: 5,
+
+    
   },
   title: {
     fontSize: 20,
